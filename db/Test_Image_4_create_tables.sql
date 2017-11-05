a1c3484fd9c74e7edba0562c909788b89b56a90e
ukimiawz <deathmoon123>
2017-11-05 16:27:28 +0100

    Generated microservice version 1.0

---
+++ db/Test_Image_4_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  musicDb
+-- Automatically generated sql script for the service Test Image 4, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tblImage.
+--
+CREATE TABLE musicDb.tblImage (
+  imageName VARCHAR(255) ,
+  imageId VARCHAR(255) ,
+CONSTRAINT imageId_PK PRIMARY KEY (imageId)
+ 
+);
+
+
+
---
+++ src/main/i5/las2peer/services/testImage4/testImage4.java
@@ -0,0 +1,5 @@
+package i5.las2peer.services.testImage4;
+
+import java.util.*;
+
+$Classes_Body$
